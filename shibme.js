fe6fcc937f14bf41fb85a76f08361156111fc04f
Kyle Skrinak <kyle@skrinakcreative.com>
2016-01-14 16:32:40 -0500

    Clicking on the bookmarklet will simply redirect your page now
    Before I threw up a dialog for copying the address. Clicking now will simple redirect you as needed.

--- shibme.js
+++ shibme.js
@@ -4,5 +4,6 @@ if (window.location.protocol !== "https:") {
 } else {
     qPathName = (window.location.pathname === '/' ? "/" + window.location.search.substring(3) : window.location.pathname);
     shibUrl = window.location.origin + "/Shibboleth.sso/Login?target=" + window.location.origin + "/shib_login" + qPathName;
-    window.prompt("Copy this Shibbolized URL to your clipboard", shibUrl);
+    //window.prompt("Copy this Shibbolized URL to your clipboard", shibUrl);
+    window.location.href =  shibUrl;
 }
